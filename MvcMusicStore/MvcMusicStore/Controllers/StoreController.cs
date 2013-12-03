0611ab31ce1b353dc0eae5f38bd1006f14b7909f
Mafalda Landeiro <1110590@isep.ipp.pt>
2013-12-03 13:06:54 +0000

    Projeto MusicStoreMvc4

---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        /*public string Index()
+        {
+            return "Hello from Store.Index()";
+        }*/
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>{
+                new Genre{Name="Disco"}, new Genre{Name="Jazz"},new Genre{Name="Rock"}
+            };
+
+            return View(genres);
+        }
+
+
+        //
+        // GET: /Store/Browse
+        /*public string Browse(string genre)
+        {
+            return HttpUtility.HtmlEncode("Store.Brower,Genre ="+genre);
+        //    return "Hello from Store.Browse()";
+        }*/
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+
+
+        //
+        // GET: /Store/Details
+        /*public string Details()
+        {
+            return "Hello from Store.Details()";
+        }*/
+
+        // GET: /Store/Details/5
+        /*public string Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+
+            return message;
+        }*/
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album" + id };
+            return View(album);
+        }
+
+    }
+}
